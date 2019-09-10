e8f855909d07ca0bf0ca1de25673872a37b6e6b9
Benedict Hartmann <bene.hartmann@web.de>
2019-09-10 21:00:47 +0200

    adding env(Toolset)

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,11 @@ class Pattern(ConanFile):
         self.build_requires("Catch2/2.7.0@catchorg/stable")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["CMAKE_TOOLCHAIN_FILE"]="./conan_paths.cmake"
+        cmake = CMake(self, toolset=self.env["compiler.toolset"])
+        cmake.definitions["CMAKE_TOOLCHAIN_FILE"]=os.path.join(self.install_folder,"conan_paths.cmake")
         cmake.configure()
         cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
