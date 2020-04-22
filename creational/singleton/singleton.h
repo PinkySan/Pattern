f20e03cd0e4e05a35585248189887e59eeca5eea
Benedict Hartmann <bene.hartmann@web.de>
2020-04-22 19:32:42 +0200

    adding creational/singleton

---
+++ creational/singleton/singleton.h
@@ -0,0 +1,16 @@
+﻿// singleton.h : Header file for your target.
+
+#pragma once
+#include <memory>
+
+class singleton
+{
+  private:
+    singleton() = default;
+    static std::shared_ptr<singleton> _instance;
+
+  public:
+    static std::shared_ptr<singleton> createInstance();
+    singleton(const singleton&) = delete;
+    void operator=(const singleton &) = delete;
+};
---
+++ creational/singleton/test/singletonTester.cpp
@@ -0,0 +1,9 @@
+﻿
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <singleton.h>
+
+TEST_CASE("BasicUsage")
+{
+    //std::shared_ptr<singleton> singleton1 = singleton::createInstance();
+}
